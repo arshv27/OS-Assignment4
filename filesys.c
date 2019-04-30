cf78371ffa7fdd7a5986d4e338d17a6b02bd7a9d
arshv27 <arshverma@gmail.com>
2019-04-30 22:41:17 +0530

    All tests pass!

--- filesys.c
+++ filesys.c
@@ -20,23 +20,21 @@ int merkel_tree(const char *pathname, int flags, mode_t mode, int ff) {
 	char temp_hash[2000][21];
 	int temp_fd = 0;
 	for (int i = 0; i < 2000; i++) {
+		if(ff){return 1;}
 		temp_hash[i][20] = '\0';
 	}
-	// char tt = temp_hash[0][20];
-	// if(tt != '\0'){
-	// 	return -1;
-	// }
-	// temp_hash[0][0] = 'f';
-	printf("reached line 30\n");
+	
+	// printf("reached line 30\n");
 	for (int i=0; i<2000; i++) {
 		data[i][20]=temp_hash[i][20];
 	}
+	
 	FILE *fp = fopen(pathname, "r");
 	int sz = 0;
 	if (fp != NULL) {
 		fseek(fp, 0, SEEK_END);
 		sz = ftell(fp);
-		printf("file name = %s, file size = %d\n", pathname, sz);
+		// printf("file name = %s, file size = %d\n", pathname, sz);
 		fseek(fp, 0, SEEK_SET);
 		fclose(fp);
 		if(sz == 0){
@@ -102,34 +100,27 @@ int merkel_tree(const char *pathname, int flags, mode_t mode, int ff) {
 		}
 		ctr = index-1;
 	}
-	printf("final hash is %s\n", data[0]);
+	// printf("final hash is %s\n", data[0]);
 
 	if(ff){
 		char arr[9][31];
 		for (int i=0; i<9; i++) {
 			arr[i][30]='\0';
 		}
-		for (int i = 0; i < 1025; i++) {
-			name_array[i][9] = '\0';
-		}
-		for (int i = 0; i < 1025; i++) {
-			for(int j = 0; j < 2000; j++) {
-				hash_array[i][j][9] = '\0';
-			}
-		}
-		if (access("secure.txt",F_OK) == -1) {
-			return 0;
-		}
-
-
-		FILE *fp = fopen("secure.txt", "r");
+		
+		FILE *fpp = fopen("secure.txt", "r");
 		char *line = NULL;
 		size_t len = 0;
 		ssize_t read = 0;
 		int i = 0;
 		int flg = 0;
 		int hasfile = 0;
-		while ((read = getline(&line, &len, fp)) != -1) {
+		exit(0);
+		read = getline(&line, &len, fpp);
+		//printf("%s\n", line);
+		while ((read = getline(&line, &len, fpp)) != -1) {
+			//printf("HERE NOW!!, i: %d\n", i);
+			// exit(0);
 			char *name = strtok(line, " ");
 			char *hash1 = strtok(NULL, " ");
 			char *hash = strtok(hash1, "\n");
@@ -139,24 +130,21 @@ int merkel_tree(const char *pathname, int flags, mode_t mode, int ff) {
 				hasfile = 1;
 			}
 
-			if (access(name,F_OK) == -1) {
-				;
-			}else{
-				for (int j = 0; j < 9; j++) {
-					arr[i][j] = name[j];
-				}
-				arr[i][9] = ' ';
-				for (int j = 10; j<30; j++) {
-					if(flg == 1){
-						arr[i][j] = data[0][j - 10];
-					}else{
-						arr[i][j] = hash[j-10];
-					}
-				}
-				i++;
+			for (int j = 0; j < 9; j++) {
+				arr[i][j] = name[j];
 			}
+			arr[i][9] = ' ';
+			for (int j = 10; j<30; j++) {
+				if(flg == 1){
+					arr[i][j] = data[0][j - 10];
+				}else{
+					arr[i][j] = hash[j-10];
+				}
+			}
+			i++;
 		}
-		fclose(fp);
+		free(line);
+		fclose(fpp);
 		if (hasfile) {
 			remove("secure.txt");
 		}
@@ -166,12 +154,11 @@ int merkel_tree(const char *pathname, int flags, mode_t mode, int ff) {
 		if(!hasfile){
 			fprintf(fp1, "%s\n", pathname);
 			fprintf(fp1, "%s", " ");
-			printf("%s\n", data[0]);
-			printf("%d\n", sz);
+			//printf("%s\n", data[0]);
+			//printf("%d\n", sz);
 			fprintf(fp1, "%s\n", data[0]);
 			fflush(fp1);
 		} else {
-
 			for (int j=0; j<i; j++) {
 				char name[10];
 				name[9]='\0';
@@ -195,7 +182,7 @@ int merkel_tree(const char *pathname, int flags, mode_t mode, int ff) {
 		return 1;
 	}
 
-	printf("reached line 101\n");
+	// printf("reached line 101\n");
 	FILE *fp1 = fopen("secure.txt", "a+");
 	int has_file = 0;
 
@@ -222,7 +209,7 @@ int merkel_tree(const char *pathname, int flags, mode_t mode, int ff) {
 			}
 		}
 	}
-	printf("reached line 101\n");
+	// printf("reached line 101\n");
 	if (!has_file) {
 		for (int i = 0; i < 2000; i++) {
 			for (int j = 0; j < 20; j++) {
@@ -231,14 +218,13 @@ int merkel_tree(const char *pathname, int flags, mode_t mode, int ff) {
 			// printf("%s\n", hash_array[temp_fd][i]);
 		}
 		fprintf(fp1, "%s ", pathname);
-		printf("%s\n", data[0]);
+		//printf("%s\n", data[0]);
 		fprintf(fp1, "%s\n", data[0]);
-		printf("%d\n", sz);
+		//printf("%d\n", sz);
 		fflush(fp1);
 		fclose(fp1);
 	}
 	return 1;
-
 }
 
 /* returns 20 bytes unique hash of the buffer (buf) of length (len)
@@ -262,6 +248,7 @@ int s_open (const char *pathname, int flags, mode_t mode)
 	assert (filesys_inited);
 	int c = merkel_tree(pathname, flags, mode, 0);
 	if (c == -1) {
+		// printf("could not open the goddamn file :/\n");
 		return -1;
 	} else {
 		int fd = open (pathname, flags, mode);
@@ -284,124 +271,6 @@ int s_lseek (int fd, long offset, int whence)
 	return lseek (fd, offset, SEEK_SET);
 }
 
-// void update_hash(const char *pathname, int flags, mode_t mode){
-// 	char data[2000][21];
-	
-// 	// char tt = temp_hash[0][20];
-// 	// if(tt != '\0'){
-// 	// 	return -1;
-// 	// }
-// 	// temp_hash[0][0] = 'f';
-// 	for (int i=0; i<2000; i++) {
-// 		data[i][20]=temp_hash[i][20];
-// 	}
-// 	FILE *fp = fopen(pathname, "r");
-// 	int sz = 0;
-// 	if (fp != NULL) {
-// 		fseek(fp, 0, SEEK_END);
-// 		sz = ftell(fp);
-// 		fseek(fp, 0, SEEK_SET);
-// 		fclose(fp);
-// 	}else{
-// 		return 1;
-// 	}
-
-// 	int fd = open(pathname, O_RDONLY, 0);
-// 	temp_fd = fd;
-// 	lseek(fd, 0, SEEK_SET);
-// 	int ptr = 0;
-// 	int ctr = 0;
-// 	while(ptr < sz){
-// 		char d[65];
-// 		d[64]='\0';
-// 		char hash[21];
-// 		if (sz-ptr>=64) {
-// 			read(fd, d, 64);
-// 		}
-// 		else{
-// 			char tmp[sz-ptr];
-// 			char tmp2[64-sz+ptr];
-// 			read(fd,tmp, sz-ptr);
-// 			read('a',tmp2,64-sz+ptr);
-// 			strcat(d,tmp);
-// 			strcat(d,tmp2);
-// 		}
-// 		get_sha1_hash(d, 64, hash);
-// 		for (int k = 0; k < 20; k++) {
-// 			temp_hash[ctr][k] = hash[k];
-// 		}
-// 		strncpy(data[ctr], hash, 20);
-// 		ptr += 64;
-// 		ctr++;
-// 	}
-// 	close(fd);
-// 	int c=0;
-// 	int index = 0;
-// 	while(ctr>0){
-// 		index = 0;
-// 		c = 0;
-// 		while (c <= ctr) {			
-// 			if (c+1 > ctr) {
-// 				get_sha1_hash(data[c], 20, data[index]);
-// 				index++;
-// 				c++;
-// 			} else {
-// 				char conchash[41];
-// 				conchash[40]='\0';
-// 				for (int i=0; i<20; i++) {
-// 					conchash[i]=data[c][i];
-// 				}
-// 				for (int i=20; i<40; i++) {
-// 					conchash[i]=data[c+1][i-20];
-// 				}
-// 				c += 2;		
-// 				get_sha1_hash(conchash, 40, data[index]);
-// 				index++;			
-// 			}
-// 		}
-// 		ctr = index-1;
-// 	}
-
-// 	FILE *fp1 = fopen("secure.txt", "a+");
-// 	int has_file = 0;
-
-// 	char *line = NULL;
-// 	size_t len = 0;
-// 	ssize_t read = 0;
-// 	while ((read = getline(&line, &len, fp1)) != -1) {
-// 		char *name = strtok(line, " ");
-// 		char *hash1 = strtok(NULL, " ");
-// 		char *hash = strtok(hash1, "\n");
-// 		if (!strcmp(pathname, name)) {
-// 			has_file = 1;
-// 			if (strcmp(hash, data[0]) != 0) {
-// 				// printf("%s---%s\n",hash, data[0] );
-// 				fclose(fp1);
-// 				return -1;
-// 			} 
-// 			else {
-// 				//implement this
-				
-// 				fclose(fp1);
-// 				return 1;
-// 			}
-// 		}
-// 	}
-// 	if (!has_file) {
-// 		for (int i = 0; i < 2000; i++) {
-// 			for (int j = 0; j < 20; j++) {
-// 				hash_array[temp_fd][i][j] = temp_hash[i][j];
-// 			}
-// 			// printf("%s\n", hash_array[temp_fd][i]);
-// 		}
-// 		fprintf(fp1, "%s ", pathname);
-// 		fprintf(fp1, "%s\n", data[0]);
-// 		fflush(fp1);
-// 		fclose(fp1);
-// 	}
-// 	return 1;
-// } 
-
 
 /* read the blocks that needs to be updated
  * check the integrity of the blocks
@@ -413,47 +282,19 @@ int s_lseek (int fd, long offset, int whence)
 ssize_t s_write (int fd, const void *buf, size_t count)
 {
 	assert (filesys_inited);
-	// off_t old = lseek(fd, 0, SEEK_CUR);
-
-	// char buffer[count+1];
-	// buffer[count] = '\0';
 	char * filename = name_array[fd];
-	
-	// printf("%s\n", filename);
-	// int fd1 = open(filename, O_CREAT|O_WRONLY, S_IRUSR|S_IWUSR);
-	int x = merkel_tree(filename, O_CREAT|O_WRONLY|O_RDONLY, S_IRUSR|S_IWUSR, 0);
+	int x = merkel_tree(filename, O_CREAT|O_WRONLY|O_RDONLY, S_IRUSR|S_IWUSR, 1);
 	if (x == -1)
 	{
-		printf("%s\n", filename);
-		printf("chut\n");
+		// printf("%s\n", filename);
+		// printf("chut\n");
 		return -1;
 	}
-
-	// if (fd1 == -1) {
-	// 	printf("%s\n", "fails");
-	// }
-
 	int a = write(fd, buf, count);
-	// int k = merkel_tree(filename, O_CREAT|O_WRONLY|O_RDONLY, S_IRUSR|S_IWUSR, 1);
-	// // exit(0);
-	// printf("%d\n", k);
+	// printf("a = %d\n", a);
+	// fsync(fd);
+	merkel_tree(filename, O_CREAT|O_WRONLY|O_RDONLY, S_IRUSR|S_IWUSR, 1);
 	return a;
-
-	// if (hash_array[fd][0][0] == '\0') {
-	// 	// printf("%s\n", "writing to empty file");
-	// 	// return 128;
-	
-		
-		
-	// } else {
-	// 	int x = s_read(fd, buffer, count);
-	// 	if (x == -1) {
-	// 		return -1;
-	// 	} else {
-	// 		lseek(fd, old, SEEK_SET);
-	// 		return write (fd, buf, count);
-	// 	}
-	// }
 }
 
 /* check the integrity of blocks containing the 
